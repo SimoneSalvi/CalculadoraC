e8af44a1a8fc1506412e5104a087896f0a53b7ba
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-05 16:47:05 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int tabuada;
+
+        Console.WriteLine("Qual tabuada você quer? (1 a 9)");
+        tabuada = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < 11; i++) {
+            Console.WriteLine(i + " x " + tabuada + " = " + (i*tabuada));
+
+        }
+
+    }
+}
