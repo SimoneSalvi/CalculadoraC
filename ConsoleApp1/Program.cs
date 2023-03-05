39761b885a6ab318bc9891b047e73d706d99d6c0
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-05 16:57:54 -0300

    a execucao da tabuada foi posta dentro de uma funcao

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,15 +4,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
         int tabuada;
 
         Console.WriteLine("Qual tabuada você quer? (1 a 9)");
         tabuada = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i < 11; i++) {
-            Console.WriteLine(i + " x " + tabuada + " = " + (i*tabuada));
+        fazTabuada();
 
+        void fazTabuada()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i + " x " + tabuada + " = " + (i * tabuada));
+
+            }
         }
+
 
     }
 }
